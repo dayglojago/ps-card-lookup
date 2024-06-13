9d6033f6e300088979d48afb3c70a96f4b87af44
Jago Goddard <jago@pullscription.com>
2024-06-13 01:27:16 -0700

    Initial Commit

---
+++ Pullscription Card Lookup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pullscription Card Lookup
+//
+//  Created by Jago Lourenco-Goddard on 6/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pullscription Card Lookup/Pullscription_Card_LookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pullscription_Card_LookupApp.swift
+//  Pullscription Card Lookup
+//
+//  Created by Jago Lourenco-Goddard on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Pullscription_Card_LookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
