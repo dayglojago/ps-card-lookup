e72a8e29254fc4301814e5da785947007f01dc9b
Jago Goddard <jago@pullscription.com>
2025-02-20 22:54:04 -0800

    Changed app name and various other settings until we added an iPad version, and made sure it compiled.

---
+++ Tools-iPad/Tools_iPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tools_iPadApp.swift
+//  Tools-iPad
+//
+//  Created by Jago Lourenco-Goddard on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Tools_iPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tools-iPadTests/Tools_iPadTests.swift
@@ -0,0 +1,16 @@
+//
+//  Tools_iPadTests.swift
+//  Tools-iPadTests
+//
+//  Created by Jago Lourenco-Goddard on 2/20/25.
+//
+
+import Testing
+
+struct Tools_iPadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
